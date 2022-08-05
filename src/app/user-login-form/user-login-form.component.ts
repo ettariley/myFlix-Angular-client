ed08f7fe7514cf0a46f2fa4eede09bbc0c65a4a7
Etta Baker <etta.r.baker@gmail.com>
2022-08-05 13:03:16 -0400

    Updated welcome page and related dialogs

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -9,28 +10,33 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
+  isLoggingIn: boolean = false;
 
   @Input() userData = { Username: '', Password: '' };
   
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router,
   ) { }
 
   ngOnInit(): void {
   }
 
   loginUser(): void {
+    this.isLoggingIn = true;
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-      this.dialogRef.close(); // This will close the modal on success!
-      localStorage.setItem('username', result.username);
+      this.isLoggingIn = false;
+      this.dialogRef.close(); // Close the modal on success
+      // Add token and username to local storage
+      localStorage.setItem('user', result.user.Username);
       localStorage.setItem('token', result.token);
-      console.log(result);
-      this.snackBar.open(result, 'OK', {
-          duration: 2000
-      });
+
+      // Redirect to movies page
+      this.router.navigate(['movies']);
     }, (result) => {
+      console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
